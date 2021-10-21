e14a973b493ae719dbd5ca00d9b8f2ae098c82cd
Erik Peterson <Erik.Exists@gmail.com>
2021-10-21 16:31:25 -0700

    fea: progress bar for Brian
    (not in use yet)

--- AuxiliarFunctions.py
+++ AuxiliarFunctions.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 from scipy import integrate
 from scipy import signal
@@ -14,6 +15,30 @@ If you use this code, please cite:
 !!!!!!!!
 
 """
+
+
+class ProgressBar(object):
+    def __init__(self, toolbar_width=40):
+        self.toolbar_width = toolbar_width
+        self.ticks = 0
+
+    def __call__(self, elapsed, complete, start, duration):
+        if complete == 0.0:
+            # setup toolbar
+            sys.stdout.write("[%s]" % (" " * self.toolbar_width))
+            sys.stdout.flush()
+            sys.stdout.write(
+                "\b" *
+                (self.toolbar_width + 1))  # return to start of line, after '['
+        else:
+            ticks_needed = int(round(complete * self.toolbar_width))
+            if self.ticks < ticks_needed:
+                sys.stdout.write("-" * (ticks_needed - self.ticks))
+                sys.stdout.flush()
+                self.ticks = ticks_needed
+        if complete == 1.0:
+            sys.stdout.write("\n")
+
 
 ################################################################################
 #External drive time dependence
