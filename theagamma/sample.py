d02c3b6b0dd37721ba04b349da1b1385c74f1f8e
Erik Peterson <Erik.Exists@gmail.com>
2021-11-30 13:55:34 -0800

    fea: sample l2 errors

--- theagamma/sample.py
+++ theagamma/sample.py
@@ -2,23 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import fire
-import sys
 import pandas as pd
-import os
 import numpy as np
 
-from joblib import Parallel, delayed
-from itertools import product
 from collections import defaultdict
 
 from theoc.oc import load_result
-from theoc.metrics import discrete_dist
+from theoc.metrics import l2_error
 from theoc.metrics import discrete_entropy
 from theoc.metrics import discrete_mutual_information
 from theoc.metrics import normalize
 
 from theagamma.util import select_n
-from theagamma.util import bin_times
 from theagamma.util import to_spikemat
 
 
@@ -32,6 +27,8 @@ def run(output_name, n, *file_names):
     H = defaultdict(list)
     MI = defaultdict(list)
     dMI = defaultdict(list)
+    l2 = defaultdict(list)
+    dl2 = defaultdict(list)
 
     # Subsample files and do MI on each
     for i, file_name in enumerate(file_names):
@@ -50,6 +47,7 @@ def run(output_name, n, *file_names):
         y = normalize(mat.sum(1))
         MI["stim_p"].append(discrete_mutual_information(y_ref, y, m))
         H["stim_p"].append(discrete_entropy(y, m))
+        l2["stim_p"].append(l2_error(y_ref, y))
 
         # E calcs
         ts, ns = res["spikes"]["E"]
@@ -61,21 +59,29 @@ def run(output_name, n, *file_names):
         y = normalize(mat.sum(1))
         MI["E"].append(discrete_mutual_information(y_ref, y, m))
         H["E"].append(discrete_entropy(y, m))
+        l2["E"].append(l2_error(y_ref, y))
 
         # Delta MI
         dMI["stim_p"].append(MI["stim_p"][-1] - MI["stim_p"][-1])
         dMI["E"].append(MI["E"][-1] - MI["stim_p"][-1])
+        dl2["E"].append(l2["E"][-1] - l2["stim_p"][-1])
 
         # Metadata
         H["file_index"].append(i)
         MI["file_index"].append(i)
         dMI["file_index"].append(i)
+        l2["file_index"].append(i)
+        dl2["file_index"].append(i)
         H["file_name"].append(file_name)
         MI["file_name"].append(file_name)
         dMI["file_name"].append(file_name)
+        l2["file_name"].append(file_name)
+        dl2["file_name"].append(file_name)
         H["num_pop"].append(n)
         MI["num_pop"].append(n)
         dMI["num_pop"].append(n)
+        l2["num_pop"].append(n)
+        dl2["num_pop"].append(n)
 
     # -- Dump to disk!
     df_H = pd.DataFrame(H)
@@ -84,6 +90,10 @@ def run(output_name, n, *file_names):
     df_MI.to_csv(output_name + f"_{n}_MI.csv", index=False)
     df_dMI = pd.DataFrame(dMI)
     df_dMI.to_csv(output_name + f"_{n}_dMI.csv", index=False)
+    df_l2 = pd.DataFrame(l2)
+    df_l2.to_csv(output_name + f"_{n}_l2.csv", index=False)
+    df_dl2 = pd.DataFrame(dl2)
+    df_dl2.to_csv(output_name + f"_{n}_dl2.csv", index=False)
 
 
 if __name__ == "__main__":
